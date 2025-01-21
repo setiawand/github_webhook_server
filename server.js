b7b1acf9f0081231ea7477dd55a67156ef08137d
Deni Setiawan <deni.setiawan@live.com>
2025-01-21 23:02:39 +0700

    update server.js add clone resolution

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { exec } = require('child_process');
 const crypto = require('crypto');
+const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json({ verify: verifySignature }));
 
 const GITHUB_SECRET = process.env.GITHUB_SECRET;
 const DEPLOYMENT_PATH = process.env.DEPLOYMENT_PATH;
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 function verifySignature(req, res, buf) {
   const signature = `sha256=${crypto
@@ -25,18 +27,45 @@ app.post('/webhook', (req, res) => {
   const event = req.headers['x-github-event'];
   if (event === 'push') {
     console.log('Push event received. Deploying...');
-    exec(`cd ${DEPLOYMENT_PATH} && git pull && docker-compose up --build -d`, (err, stdout, stderr) => {
-      if (err) {
-        console.error(`Error during deploy: ${stderr}`);
-        return res.status(500).send('Deploy failed');
-      }
-      console.log(`Deploy success: ${stdout}`);
-      res.status(200).send('Deploy successful');
-    });
+    
+    // Ambil REPO_URL dari payload
+    const repoUrl = req.body.repository.clone_url.replace(
+      'https://github.com',
+      `https://${GITHUB_TOKEN}@github.com`
+    );
+
+    console.log(`Repository URL: ${repoUrl}`);
+
+    // Cek apakah folder repository ada
+    if (!fs.existsSync(DEPLOYMENT_PATH)) {
+      console.log('Repository not found. Cloning...');
+      exec(`git clone ${repoUrl} ${DEPLOYMENT_PATH}`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(`Error during clone: ${stderr}`);
+          return res.status(500).send('Clone failed');
+        }
+        console.log(`Clone success: ${stdout}`);
+        deployRepository(res);
+      });
+    } else {
+      deployRepository(res);
+    }
   } else {
     res.status(200).send('Event ignored');
   }
 });
+
+// Deploy repository (git pull and docker-compose)
+function deployRepository(res) {
+  exec(`cd ${DEPLOYMENT_PATH} && git pull && docker-compose up --build -d`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error during deploy: ${stderr}`);
+      return res.status(500).send('Deploy failed');
+    }
+    console.log(`Deploy success: ${stdout}`);
+    res.status(200).send('Deploy successful');
+  });
+}
 
 // Start server
 app.listen(3000, () => {
